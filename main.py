6385c495e63547505e02f3503a81a39abacc8290
starwalker00 <aaronwalkerup@gmail.com>
2022-09-28 21:34:40 +0200

    feat: connect to ethereum

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
+import os
+from dotenv import load_dotenv
+from web3 import Web3
+
 def main():
     print("main()")
+    load_dotenv()
+    alchemy_url = os.environ['HTTP_PROVIDER_URL']
+    w3 = Web3(Web3.HTTPProvider(alchemy_url))
+    print(w3.isConnected())
 
-if __name__ == "__main__":
+if __name__ == "__main__":   
     main()
